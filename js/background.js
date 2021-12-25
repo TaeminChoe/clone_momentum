7501f4c2c86d751453f40efbbf6dd656430cc41d
TaeminChoe <taeminteng@gmail.com>
2021-12-26 01:12:58 +0900

    make todo list function

--- js/background.js
+++ js/background.js
@@ -10,4 +10,3 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement('img');
 bgImage.src = ` img/${chosenImage}`;
 document.querySelector('body').appendChild(bgImage);
-console.log(bgImage);
---
+++ js/todo.js
@@ -0,0 +1,58 @@
+const toDoForm = document.querySelector('#todo-form');
+const toDoInput = document.querySelector('#todo-form input');
+const toDoList = document.querySelector('#todo-list');
+
+const TODOS_KEY = 'todos';
+
+let toDos = [];
+
+function handleToDoSubmit(event) {
+	event.preventDefault();
+	const newTodo = {
+		id: Date.now(),
+		value: toDoInput.value,
+	};
+	toDoInput.value = '';
+	createTodo(newTodo);
+	toDos.push(newTodo);
+	updateToDos();
+}
+
+function delToDO(event) {
+	const parentLi = event.target.parentElement;
+	toDos = toDos.filter(todo => todo.id !== Number(parentLi.id));
+	parentLi.remove();
+	updateToDos();
+}
+
+function updateToDos() {
+	localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function loadToDos() {
+	const parsedToDos = JSON.parse(localStorage.getItem(TODOS_KEY));
+	toDos = parsedToDos;
+	parsedToDos.forEach(createTodo);
+}
+
+function createTodo(newTodo) {
+	const li = document.createElement('li');
+	const span = document.createElement('span');
+	const btn = document.createElement('button');
+	const now = Date.now();
+	span.innerText = newTodo.value;
+	btn.innerText = 'X';
+	li.id = newTodo.id;
+
+	btn.addEventListener('click', delToDO);
+	li.appendChild(span);
+	li.appendChild(btn);
+	toDoList.appendChild(li);
+}
+
+toDoForm.onsubmit = handleToDoSubmit;
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos) {
+	loadToDos();
+}
