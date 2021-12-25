3ee92d0daf6b5c1e5c5df2e8560cd13b53ac16af
TaeminChoe <taeminteng@gmail.com>
2021-12-25 23:12:59 +0900

    make background

---
+++ js/background.js
@@ -0,0 +1,13 @@
+const images = [
+	'background01.jpg',
+	'background02.jpg',
+	'background03.jpg',
+	'background04.jpg',
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement('img');
+bgImage.src = ` img/${chosenImage}`;
+document.querySelector('body').appendChild(bgImage);
+console.log(bgImage);
